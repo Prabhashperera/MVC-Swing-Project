106902fa508d87b9624d86cd34835d883d1e4efc
Prabhashperera <prabashmperera01@gmail.com>
2024-09-11 13:02:45 +0530

    Customer Model Delete Customer

--- src/model/CustomerModel.java
+++ src/model/CustomerModel.java
@@ -33,7 +33,7 @@ public class CustomerModel {
         return rows < 0 ? "Success" : "Failed";
        
     }
-        public String updateCustomer(CustomerDto dto) throws ClassNotFoundException, SQLException {
+    public String updateCustomer(CustomerDto dto) throws ClassNotFoundException, SQLException {
         Connection conn = DBConnection.getInstance().getConnection();
         String query = "UPDATE Customer CustTitle = ?, CustName = ?, DOB = ? , salary = ?, "
                 + "CustAddress = ?, City = ?, Province = ? , PostalCode = ? WHERE CustID = ? ";
@@ -55,4 +55,27 @@ public class CustomerModel {
         return rows < 0 ? "Success" : "Failed";
        
     }
+        public String deleteCustomer(CustomerDto dto) throws ClassNotFoundException, SQLException {
+        Connection conn = DBConnection.getInstance().getConnection();
+        String query = "DELETE Customer CustTitle = ?, CustName = ?, DOB = ? , salary = ?, "
+                + "CustAddress = ?, City = ?, Province = ? , PostalCode = ? WHERE CustID = ? ";
+        
+        PreparedStatement stm = conn.prepareStatement(query);
+        stm.setString(1, dto.getTitle());
+        stm.setString(2, dto.getName());
+        stm.setString(3, dto.getDob());
+        stm.setDouble(4, dto.getSalary());
+        stm.setString(5, dto.getAddress());
+        stm.setString(6, dto.getCity());
+        stm.setString(7, dto.getProvince());
+        stm.setString(8, dto.getPostalCode());
+        stm.setString(9, dto.getId());
+
+        
+        int rows = stm.executeUpdate();
+        
+        return rows < 0 ? "Success" : "Failed";
+       
+    }
+    
 }
